3bd26a1e3cfe325efea2e2737a2bc8b34ba28e4b
Jilles Soeters <jilles.soeters@udemy.com>
2018-01-08 13:37:49 +0000

    Clean up code a bit more before writing tests

--- dist/videojs-sneakpeek.js
+++ dist/videojs-sneakpeek.js
@@ -52,10 +52,10 @@ function parseImageLink(imglocation) {
     if (hashindex === -1) {
         return {
             src: imglocation,
-            w: 0,
-            h: 0,
             x: 0,
-            y: 0
+            y: 0,
+            w: 0,
+            h: 0
         };
     }
 
@@ -64,56 +64,61 @@ function parseImageLink(imglocation) {
     if (hashstring.substring(0, 5) !== 'xywh=') {
         return {
             src: defaults.basePath + lsrc,
-            w: 0,
-            h: 0,
             x: 0,
-            y: 0
+            y: 0,
+            w: 0,
+            h: 0
         };
     }
 
     var data = hashstring.substring(5).split(',');
     return {
         src: defaults.basePath + lsrc,
-        w: +data[2],
-        h: +data[3],
         x: +data[0],
-        y: +data[1]
+        y: +data[1],
+        w: +data[2],
+        h: +data[3]
     };
 }
 
 _video2.default.plugin('sneakpeek', function (options) {
-    var tracks = void 0,
-        thumbTrack = void 0;
     defaults.basePath = options.basePath || defaults.basePath;
+
     var settings = _extends({}, defaults, options);
     var player = this;
+
+    var tracks = [],
+        sneakpeekTrack = void 0;
 
     player.ready(function () {
         tracks = player.remoteTextTracks();
         checkForTracks();
-        if (!thumbTrack) {
+        if (!sneakpeekTrack) {
             tracks.addEventListener('addtrack', checkForTracks);
         }
     });
 
     function checkForTracks() {
-        if (thumbTrack) {
+        if (sneakpeekTrack) {
             return;
         }
+
+        // Tracks is an Array-like object so we can't use .find
         for (var i = 0; i < tracks.length; i++) {
             if (tracks[i].kind === 'metadata') {
-                thumbTrack = tracks[i];
+                sneakpeekTrack = tracks[i];
                 // Chrome needs this
-                thumbTrack.mode = 'hidden';
+                sneakpeekTrack.mode = 'hidden';
                 break;
             }
         }
-        if (!thumbTrack) {
+
+        if (!sneakpeekTrack) {
             return;
         }
 
         // Android doesn't support :active and :hover on non-anchor and non-button elements
-        // so, we need to fake the :active selector for thumbnails to show up.
+        // so, we need to fake the :active selector for the sneakpeek to show up.
         if (navigator.userAgent.toLowerCase().indexOf('android') !== -1) {
             var _progressControl = player.controlBar.progressControl;
 
@@ -129,47 +134,44 @@ _video2.default.plugin('sneakpeek', function (options) {
             _progressControl.on('touchcancel', removeFakeActive);
         }
 
-        // create the thumbnail
-        var div = document.createElement('div');
-        div.className = 'vjs-thumbnail-holder';
+        var container = document.createElement('div');
+        container.className = 'vjs-sneakpeek-holder';
         var img = document.createElement('img');
-        div.appendChild(img);
-        img.className = 'vjs-thumbnail';
+        container.appendChild(img);
+        img.className = 'vjs-sneakpeek';
 
-        // keep track of the duration to calculate correct thumbnail to display
         var duration = player.duration();
 
-        // when the container is MP4
+        // MP4 time change
         player.on('durationchange', function () {
             duration = player.duration();
         });
 
-        // when the container is HLS
+        // HLS time change
         player.on('loadedmetadata', function () {
             duration = player.duration();
         });
 
-        // add the thumbnail to the player
+        // Add the sneakpeek
         var progressControl = player.controlBar.progressControl;
-        progressControl.el().appendChild(div);
+        progressControl.el().appendChild(container);
 
-        /* eslint max-statements: ["error", 45] */
         function moveListener(event) {
             var pageXOffset = getScrollOffset().x;
             var clientRect = offsetParent(progressControl.el()).getBoundingClientRect();
-            var right = (clientRect.width || clientRect.right) + pageXOffset;
 
             var pageX = event.pageX;
             if (event.changedTouches) {
                 pageX = event.changedTouches[0].pageX;
             }
 
-            // find the page offset of the mouse
+            // Find the page offset of the mouse
+            var right = (clientRect.width || clientRect.right) + pageXOffset;
             var left = pageX || event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            // subtract the page offset of the positioned offset parent
+            // Subtract the page offset of the positioned offset parent
             left -= offsetParent(progressControl.el()).getBoundingClientRect().left + pageXOffset;
 
-            // apply updated styles to the thumbnail if necessary
+            // Apply updated styles to the sneakpeek if necessary
             // mouseTime is the position of the mouse along the progress control bar
             // `left` applies to the mouse position relative to the player so we need
             // to remove the progress control's left offset to know the mouse position
@@ -177,58 +179,54 @@ _video2.default.plugin('sneakpeek', function (options) {
             var mouseTime = Math.floor((left - progressControl.el().offsetLeft) / progressControl.width() * duration);
 
             // Now check which of the cues applies
-            var cnum = thumbTrack && thumbTrack.cues.length;
+            var cueLength = sneakpeekTrack && sneakpeekTrack.cues.length;
             var i = 0;
-            var setting = void 0;
-            while (i < cnum) {
-                var ccue = thumbTrack.cues[i];
-                if (ccue.startTime <= mouseTime && ccue.endTime >= mouseTime) {
-                    setting = parseImageLink(ccue.text);
+            var imageSettings = void 0;
+            while (i < cueLength) {
+                var cue = sneakpeekTrack.cues[i];
+                if (cue.startTime <= mouseTime && cue.endTime >= mouseTime) {
+                    imageSettings = parseImageLink(cue.text);
                     break;
                 }
                 i++;
             }
 
             // None found, so show nothing
-            if (typeof setting === 'undefined') {
+            if (typeof imageSettings === 'undefined') {
                 return;
             }
 
             // Changed image?
-            if (setting.src && img.src !== setting.src) {
+            if (imageSettings.src && img.src !== imageSettings.src) {
                 var baseUrl = '';
                 // Make the
-                if (setting.src[0] === '/') {
+                if (imageSettings.src[0] === '/') {
                     baseUrl = player.currentSrc();
                     baseUrl = baseUrl.substring(0, baseUrl.lastIndexOf('/'));
                 }
-                img.src = baseUrl + setting.src;
+                img.src = baseUrl + imageSettings.src;
             }
 
             // Fall back to plugin defaults in case no height/width is specified
-            if (setting.w === 0) {
-                setting.w = settings.width;
-            }
+            imageSettings.w = imageSettings.w || settings.width;
+            imageSettings.h = imageSettings.h || settings.height;
 
-            if (setting.h === 0) {
-                setting.h = settings.height;
-            }
-
-            // Set the container width/height if it changed
-            if (div.style.width !== setting.w || div.style.height !== setting.h) {
-                div.style.width = setting.w + 'px';
-                div.style.height = setting.h + 'px';
+            // Set the container width / height if it changed
+            if (container.style.width !== imageSettings.w || container.style.height !== imageSettings.h) {
+                container.style.width = imageSettings.w + 'px';
+                container.style.height = imageSettings.h + 'px';
             }
 
             // Set the image cropping
-            img.style.left = -setting.x + 'px';
-            img.style.top = -setting.y + 'px';
-            img.style.clip = 'rect(' + setting.y + 'px,' + (setting.w + setting.x) + 'px,' + (setting.y + setting.h) + 'px,' + setting.x + 'px)';
+            img.style.left = -imageSettings.x + 'px';
+            img.style.top = -imageSettings.y + 'px';
+            var clipOffsets = [imageSettings.y, imageSettings.w + imageSettings.x, imageSettings.y + imageSettings.h, imageSettings.x];
+            img.style.clip = 'rect(' + clipOffsets.join('px,') + 'px)';
 
-            var width = setting.w;
+            var width = imageSettings.w;
             var halfWidth = width / 2;
 
-            // make sure that the thumbnail doesn't fall off the right side of the left side of the player
+            // make sure that the sneakpeek doesn't fall off the right side of the left side of the player
             if (left + halfWidth > right) {
                 left = right - width;
             } else if (left < halfWidth) {
@@ -237,16 +235,16 @@ _video2.default.plugin('sneakpeek', function (options) {
                 left = left - halfWidth;
             }
 
-            div.style.left = left + 'px';
+            container.style.left = left + 'px';
         }
-
-        // update the thumbnail while hovering
-        progressControl.on('mousemove', moveListener);
-        progressControl.on('touchmove', moveListener);
 
         function moveCancel() {
-            div.style.left = '-1000px';
+            container.style.left = '-1000px';
         }
+
+        // update the sneakpeek while hovering
+        progressControl.on('mousemove', moveListener);
+        progressControl.on('touchmove', moveListener);
 
         // move the placeholder out of the way when not hovering
         progressControl.on('mouseout', moveCancel);
--- src/sneakpeek.js
+++ src/sneakpeek.js
@@ -41,10 +41,10 @@ function parseImageLink(imglocation) {
     if (hashindex === -1) {
         return {
             src: imglocation,
-            w: 0,
-            h: 0,
             x: 0,
             y: 0,
+            w: 0,
+            h: 0,
         };
     }
 
@@ -53,49 +53,60 @@ function parseImageLink(imglocation) {
     if (hashstring.substring(0, 5) !== 'xywh=') {
         return {
             src: defaults.basePath + lsrc,
-            w: 0,
-            h: 0,
             x: 0,
             y: 0,
+            w: 0,
+            h: 0,
         };
     }
 
     const data = hashstring.substring(5).split(',');
     return {
         src: defaults.basePath + lsrc,
-        w: +data[2],
-        h: +data[3],
         x: +data[0],
         y: +data[1],
+        w: +data[2],
+        h: +data[3],
     };
 }
 
 videojs.plugin('sneakpeek', function (options) {
-    let tracks, thumbTrack;
     defaults.basePath = options.basePath || defaults.basePath;
+
     const settings = Object.assign({}, defaults, options);
     const player = this;
+
+    let tracks = [], sneakpeekTrack;
 
     player.ready(() => {
         tracks = player.remoteTextTracks();
         checkForTracks();
-        if (!thumbTrack) { tracks.addEventListener('addtrack', checkForTracks); }
+        if (!sneakpeekTrack) {
+            tracks.addEventListener('addtrack', checkForTracks);
+        }
     });
 
     function checkForTracks() {
-        if (thumbTrack) { return; }
+        if (sneakpeekTrack) {
+            return;
+        }
+
+        // Tracks is an Array-like object so we can't use .find
         for (let i = 0; i < tracks.length; i++) {
             if (tracks[i].kind === 'metadata') {
-                thumbTrack = tracks[i];
+                sneakpeekTrack = tracks[i];
                 // Chrome needs this
-                thumbTrack.mode = 'hidden';
+                sneakpeekTrack.mode = 'hidden';
                 break;
             }
         }
-        if (!thumbTrack) { return; }
+
+        if (!sneakpeekTrack) {
+            return;
+        }
 
         // Android doesn't support :active and :hover on non-anchor and non-button elements
-        // so, we need to fake the :active selector for thumbnails to show up.
+        // so, we need to fake the :active selector for the sneakpeek to show up.
         if (navigator.userAgent.toLowerCase().indexOf('android') !== -1) {
             const progressControl = player.controlBar.progressControl;
 
@@ -111,47 +122,44 @@ videojs.plugin('sneakpeek', function (options) {
             progressControl.on('touchcancel', removeFakeActive);
         }
 
-        // create the thumbnail
-        const div = document.createElement('div');
-        div.className = 'vjs-thumbnail-holder';
+        const container = document.createElement('div');
+        container.className = 'vjs-sneakpeek-holder';
         const img = document.createElement('img');
-        div.appendChild(img);
-        img.className = 'vjs-thumbnail';
+        container.appendChild(img);
+        img.className = 'vjs-sneakpeek';
 
-        // keep track of the duration to calculate correct thumbnail to display
         let duration = player.duration();
 
-        // when the container is MP4
+        // MP4 time change
         player.on('durationchange', () => {
             duration = player.duration();
         });
 
-        // when the container is HLS
+        // HLS time change
         player.on('loadedmetadata', () => {
             duration = player.duration();
         });
 
-        // add the thumbnail to the player
+        // Add the sneakpeek
         const progressControl = player.controlBar.progressControl;
-        progressControl.el().appendChild(div);
+        progressControl.el().appendChild(container);
 
-        /* eslint max-statements: ["error", 45] */
         function moveListener(event) {
             const pageXOffset = getScrollOffset().x;
             const clientRect = offsetParent(progressControl.el()).getBoundingClientRect();
-            const right = (clientRect.width || clientRect.right) + pageXOffset;
 
             let pageX = event.pageX;
             if (event.changedTouches) {
                 pageX = event.changedTouches[0].pageX;
             }
 
-            // find the page offset of the mouse
+            // Find the page offset of the mouse
+            const right = (clientRect.width || clientRect.right) + pageXOffset;
             let left = pageX || (event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft);
-            // subtract the page offset of the positioned offset parent
+            // Subtract the page offset of the positioned offset parent
             left -= offsetParent(progressControl.el()).getBoundingClientRect().left + pageXOffset;
 
-            // apply updated styles to the thumbnail if necessary
+            // Apply updated styles to the sneakpeek if necessary
             // mouseTime is the position of the mouse along the progress control bar
             // `left` applies to the mouse position relative to the player so we need
             // to remove the progress control's left offset to know the mouse position
@@ -159,58 +167,59 @@ videojs.plugin('sneakpeek', function (options) {
             const mouseTime = Math.floor((left - progressControl.el().offsetLeft) / progressControl.width() * duration);
 
             // Now check which of the cues applies
-            const cnum = thumbTrack && thumbTrack.cues.length;
+            const cueLength = sneakpeekTrack && sneakpeekTrack.cues.length;
             let i = 0;
-            let setting;
-            while (i < cnum) {
-                const ccue = thumbTrack.cues[i];
-                if (ccue.startTime <= mouseTime && ccue.endTime >= mouseTime) {
-                    setting = parseImageLink(ccue.text);
+            let imageSettings;
+            while (i < cueLength) {
+                const cue = sneakpeekTrack.cues[i];
+                if (cue.startTime <= mouseTime && cue.endTime >= mouseTime) {
+                    imageSettings = parseImageLink(cue.text);
                     break;
                 }
                 i++;
             }
 
             // None found, so show nothing
-            if (typeof setting === 'undefined') {
+            if (typeof imageSettings === 'undefined') {
                 return;
             }
 
             // Changed image?
-            if (setting.src && img.src !== setting.src) {
+            if (imageSettings.src && img.src !== imageSettings.src) {
                 let baseUrl = '';
                 // Make the
-                if (setting.src[0] === '/') {
+                if (imageSettings.src[0] === '/') {
                     baseUrl = player.currentSrc();
                     baseUrl = baseUrl.substring(0, baseUrl.lastIndexOf('/'));
                 }
-                img.src = baseUrl + setting.src;
+                img.src = baseUrl + imageSettings.src;
             }
 
             // Fall back to plugin defaults in case no height/width is specified
-            if (setting.w === 0) {
-                setting.w = settings.width;
-            }
+            imageSettings.w = imageSettings.w || settings.width;
+            imageSettings.h = imageSettings.h || settings.height;
 
-            if (setting.h === 0) {
-                setting.h = settings.height;
-            }
-
-            // Set the container width/height if it changed
-            if (div.style.width !== setting.w || div.style.height !== setting.h) {
-                div.style.width = `${setting.w}px`;
-                div.style.height = `${setting.h}px`;
+            // Set the container width / height if it changed
+            if (container.style.width !== imageSettings.w || container.style.height !== imageSettings.h) {
+                container.style.width = `${imageSettings.w}px`;
+                container.style.height = `${imageSettings.h}px`;
             }
 
             // Set the image cropping
-            img.style.left = `${-(setting.x)}px`;
-            img.style.top = `${-(setting.y)}px`;
-            img.style.clip = `rect(${setting.y}px,${setting.w + setting.x}px,${setting.y + setting.h}px,${setting.x}px)`;
+            img.style.left = `${-(imageSettings.x)}px`;
+            img.style.top = `${-(imageSettings.y)}px`;
+            const clipOffsets = [
+                imageSettings.y,
+                imageSettings.w + imageSettings.x,
+                imageSettings.y + imageSettings.h,
+                imageSettings.x
+            ];
+            img.style.clip = `rect(${clipOffsets.join('px,')}px)`;
 
-            const width = setting.w;
+            const width = imageSettings.w;
             const halfWidth = width / 2;
 
-            // make sure that the thumbnail doesn't fall off the right side of the left side of the player
+            // make sure that the sneakpeek doesn't fall off the right side of the left side of the player
             if ((left + halfWidth) > right) {
                 left = right - width;
             } else if (left < halfWidth) {
@@ -219,16 +228,16 @@ videojs.plugin('sneakpeek', function (options) {
                 left = left - halfWidth;
             }
 
-            div.style.left = `${left}px`;
+            container.style.left = `${left}px`;
         }
-
-        // update the thumbnail while hovering
-        progressControl.on('mousemove', moveListener);
-        progressControl.on('touchmove', moveListener);
 
         function moveCancel() {
-            div.style.left = '-1000px';
+            container.style.left = '-1000px';
         }
+
+        // update the sneakpeek while hovering
+        progressControl.on('mousemove', moveListener);
+        progressControl.on('touchmove', moveListener);
 
         // move the placeholder out of the way when not hovering
         progressControl.on('mouseout', moveCancel);
