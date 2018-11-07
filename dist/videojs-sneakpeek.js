45049fab48e9c3f0ef3ac1bc8809cfced2f4c013
Martin B <martin.bachwerk@udemy.com>
2018-11-07 12:43:06 +0000

    Make the plugin pick a specific metadata track

--- dist/videojs-sneakpeek.js
+++ dist/videojs-sneakpeek.js
@@ -135,7 +135,7 @@ function sneakpeek(options) {
 
         // Tracks is an Array-like object so we can't use .find
         for (var i = 0; i < tracks.length; i++) {
-            if (tracks[i].kind === 'metadata') {
+            if (tracks[i].kind === 'metadata' && tracks[i].label === 'sneakpeek') {
                 sneakpeekTrack = tracks[i];
                 // Chrome needs this
                 sneakpeekTrack.mode = 'hidden';
